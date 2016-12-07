40ac43f65787cbcbe39fcc6e1c897c96d2fea1ef
Rafael A. Irizarry <rairizarry@gmail.com>
2016-12-07 17:22:26 -0500

    starting new package with data science examples

---
+++ R/murders.R
@@ -0,0 +1,18 @@
+#' US murder totals from FBI reports and thpopulation of each state for 2010.
+#'
+#'
+#' @docType data
+#'
+#' @usage data(murders)
+#'
+#' @format An object of class \code{"data.frame"}. 
+#'
+#' @keywords datasets
+#'
+#'
+#' @source \href{https://en.wikipedia.org/wiki/Murder_in_the_United_States_by_state}{Wikipedia}
+#'
+#' @examples
+#' data(murders)
+#' print(murders)
+"murders"
