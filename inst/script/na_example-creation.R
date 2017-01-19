8b5612f2e61290b8c752d0932819f3f58e4599b7
Rafael A. Irizarry <rairizarry@gmail.com>
2017-01-18 23:40:27 -0500

    edits

--- inst/script/na_example-creation.R
+++ inst/script/na_example-creation.R
@@ -1,7 +1,5 @@
 ##Set working directory to package main directory
-set.seed(2)
+set.seed(8)
 na_example <- rpois(1000,2)
 na_example[na_example==0] <- NA
-sum(is.na(na_example))
-head(na_example)
 save(na_example, file = "data/na_example.rda")
