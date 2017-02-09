43b37c9cb297dd1e8cecd8b53bb3e32e533688b1
Rafael A. Irizarry <rairizarry@gmail.com>
2017-02-09 01:15:39 -0500

    addiny my_theme_set

---
+++ R/my_theme_set.R
@@ -0,0 +1,28 @@
+#' My theme set 
+#'
+#' Called without arguments, this function sets a ggplot2 theme used throughout the data science courses. 
+#'
+#' @param new a prebuilt theme. Defaults to theme_minimal
+#' @param args the arguments to be passed along to the theme function. Defaults to \code{"NULL"}.
+#' @param base_size if \code{"args"} is \code{"NULL"}, base_size is one of the arguments passed to the theme function. It defaults to 11.
+#' @param base_family if \code{"args"} is \code{"NULL"}, base_family is one of the arguments passed to the theme function. It defaults to Helvetica.
+#' @param ... additional arguments to be used by theme_set
+#'  
+#' @return None
+#'
+#' @examples
+#' library(ggplot2)
+#' my_theme_set()
+#' qplot(hp, mpg, data=mtcars, color=am, facets=gear~cyl, 
+#' main="Scatterplots of MPG vs. Horsepower", 
+#' xlab="Horsepower", ylab="Miles per Gallon")
+#'
+#' @export
+#' 
+my_theme_set <- function(new = "theme_minimal", args = NULL,
+                         base_size = 11, base_family = "Helvetica", ...){
+  requireNamespace("ggplot2")
+  if(! "package:ggplot2" %in% search() ) stop("ggplot2 must be loaded.\nLoad with library(ggplot2) or library(tidyverse).")
+  if(is.null(args)) args <- list(base_size=base_size, base_family=base_family)
+  ggplot2::theme_set(do.call(new, args), ...)
+}
