5917545f1319010ad7c6723d3a232ec94b609f3d
Rafael A. Irizarry <rairizarry@gmail.com>
2017-07-26 13:00:39 -0400

    adding divorce versus margarine data

---
+++ R/divorce_margarine.R
@@ -0,0 +1,24 @@
+#' Divorce rate and margarine consumption data
+#' 
+#' Divorce rates in Maine and per capita consumption of margarine in US data
+#' 
+#' 
+#'
+#' \itemize{
+#' divorce_rate_maine. ivorce rates in Maine
+#' margarine_consumption_per_capita. per capita consumption of margarine in US.
+#' year. Year.
+#' }
+#'
+#' @docType data
+#'
+#' @usage data(divorce_margarine)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @source \url{http://www.tylervigen.com/spurious-correlations}{Spurious Correlations}
+#' data(divorce_margarine)
+#' with(divorce_margarine, plot(argarine_consumption_per_capita, divorce_rate_maine))
+"divorce_margarine"
