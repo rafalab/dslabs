2d7f3453ac43dd6f73f42ebbc07f57167efd3a94
Rafael A. Irizarry <rairizarry@gmail.com>
2017-07-26 13:11:40 -0400

    edits

--- inst/script/make-divorce_margarine.R
+++ inst/script/make-divorce_margarine.R
@@ -1,5 +1,5 @@
 ## Extracted from http://www.tylervigen.com/spurious-correlations
-divorce_margarine<- data.frame(divorce_rate_maine = c(5, 4.7, 4.6, 4.4, 4.3, 4.1, 4.2, 4.2, 4.2, 3.7)/1000,
+divorce_margarine<- data.frame(divorce_rate_maine = c(5, 4.7, 4.6, 4.4, 4.3, 4.1, 4.2, 4.2, 4.2, 3.7),
                    margarine_consumption_per_capita = c(8.2, 7, 6.5, 5.3, 5.2, 4, 4.6, 4.5, 4.2, 4.1),
                    year = 2000:2009) 
 
