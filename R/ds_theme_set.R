3b27230929c84337eb6e18eb39f76792883c173a
Rafael A. Irizarry <rairizarry@gmail.com>
2017-09-22 11:09:41 -0400

    Removing the font definition from ds_theme_set to avoid warning on Windows Adding csv examples. Adding research funding rates rda

--- R/ds_theme_set.R
+++ R/ds_theme_set.R
@@ -5,7 +5,6 @@
 #' @param new a prebuilt ggplot2 theme. Defaults to \code{"theme_minimal"}
 #' @param args the arguments to be passed along to the ggplot2 theme function. Defaults to \code{"NULL"}.
 #' @param base_size if \code{"args"} is \code{"NULL"}, base_size is one of the arguments passed to the theme function. It defaults to 11.
-#' @param base_family if \code{"args"} is \code{"NULL"}, base_family is one of the arguments passed to the theme function. It defaults to Helvetica.
 #' @param bold_title if TRUE, sets titles to be bold
 #' @param ... additional arguments to be used by theme
 #'  
@@ -21,14 +20,14 @@
 #' @export
 #' 
 ds_theme_set <- function(new = "theme_bw", args = NULL,
-                         base_size = 11, base_family = "Helvetica", 
+                         base_size = 11, 
                          bold_title = TRUE, 
                          ...){
   requireNamespace("ggplot2")
   
   if(! "package:ggplot2" %in% search() ) stop("ggplot2 must be loaded.\nLoad with library(ggplot2) or library(tidyverse).")
   
-  if(is.null(args)) args <- list(base_size=base_size, base_family=base_family)
+  if(is.null(args)) args <- list(base_size=base_size)
   
   th <- do.call(new, args) + ggplot2::theme(...)
   ggplot2::theme_set(th)
--- inst/script/save-gapminder-example-csv.R
+++ inst/script/save-gapminder-example-csv.R
@@ -4,6 +4,17 @@ tab <- read_csv("https://docs.google.com/spreadsheet/pub?key=phAwcNAVuyj0TAlJeCE
 countries <- c("South Korea","Germany")
 names(tab)[1] <- "country"
 tab <- tab %>% filter(country %in% countries) %>% select(country,`1960`:`2015`)
-write.csv(tab, "../extdata/fertility-two-countries-example.csv", row.names = FALSE)
+write.csv(tab, "inst/extdata/fertility-two-countries-example.csv", row.names = FALSE)
 
+tab1 <- tab
+tab2 <- read_csv("http://docs.google.com/spreadsheet/pub?key=phAwcNAVuyj2tPLxKvvnNPA&output=csv")
+countries <- c("South Korea","Germany")
+names(tab2)[1] <- "country"
+tab2 <- tab2 %>% filter(country %in% countries) %>% select(country,`1960`:`2015`)
 
+colnames(tab1)[-1] <- paste(colnames(tab1)[-1], "fertility", sep="_")
+colnames(tab2)[-1] <- paste(colnames(tab2)[-1], "life_expectancy", sep="_")
+
+tab3 <- full_join(tab1, tab2, by="country")
+tab3 <- select(tab3, c("country", sort(names(tab3)[-1])))
+write.csv(tab3, "inst/extdata/life-expectancy-and-fertility-two-countries-example.csv", row.names = FALSE)
