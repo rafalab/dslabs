9860e8419dee3de9db4a93d4e9e87f734d9ae996
Rafael A. Irizarry <rairizarry@gmail.com>
2018-05-16 21:05:41 -0400

    edits to the file that makes the trump tweets data

--- inst/script/make-trump-tweets-rda.R
+++ inst/script/make-trump-tweets-rda.R
@@ -1,9 +1,14 @@
+## Download data
+## remove '^"`  because these are retweets
+## change the date a Data object
 library(tidyverse)
 library(lubridate)
 
 url <- 'http://www.trumptwitterarchive.com/data/realdonaldtrump/%s.json'
 trump_tweets <- map(2009:2017, ~sprintf(url, .x)) %>%
   map_df(jsonlite::fromJSON, simplifyDataFrame = TRUE) %>%
-  mutate(created_at = parse_date_time(created_at, orders = "a b! d! H!:M!:S! z!* Y!")) 
-
+  filter(!is_retweet & !str_detect(text, '^"')) %>%
+  mutate(created_at = parse_date_time(created_at, orders = "a b! d! H!:M!:S! z!* Y!", tz="EST")) 
+  
 save(trump_tweets, file="data/trump_tweets.rda", compress="xz")
+
