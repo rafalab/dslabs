526ad51dce37001209b9f6bdbc588cc1b85edf90
Rafael A. Irizarry <rairizarry@gmail.com>
2018-07-11 20:49:58 -0400

    Adding olives dataset

---
+++ R/olives.R
@@ -0,0 +1,32 @@
+#' Italian olive
+#' 
+#' Percentage composition of 8 fatty acids found in the lipid fraction of 572 Italian olive oils
+#' 
+#' \itemize{
+#' \item area. Area of Italy. 
+#' \item region. General region of Italy 
+#' \item palmitic. Percent of palmitic acidin sample. 
+#' \item palmitoleic. Percent of palmitoleic in sample. 
+#' \item stearic. Percent of stearic acid in sample.  
+#' \item oleic. Percent of oleic acid in sample.  
+#' \item linoleic. Percent of linoleic acid in sample.  
+#' \item linolenic. Percent of linolenic acid in sample.  
+#' \item arachidic. Percent of arachidic acid in sample.  
+#' \item eicosenoic. Percent of eicosenoic acid in sample.  
+#' }
+#'
+#' @docType data
+#'
+#' @usage data(olives)
+#'
+#' @format An object of class \code{"data.frame"}. 
+#'
+#' @keywords datasets
+#'
+#'
+#' @source J. Zupan, and J. Gasteiger.  Neural Networks in Chemistry and Drug Design.
+#' 
+#' @examples
+#' data(olives)
+#' head(olives)
+"olives"
