d5df84310468a7cebde20f2c60fa7d85fe555712
Rafael A. Irizarry <rairizarry@gmail.com>
2018-07-12 12:06:13 -0400

    Changing percent x 100 to just percent.

--- inst/script/make-olives.R
+++ inst/script/make-olives.R
@@ -1,6 +1,7 @@
 olives <- read.csv("inst/extdata/olive.csv")
 olives$Area <- factor(olives[,1])
 olives <- olives[,-1]
+olives[ , -c(1,2)] <- olives[ , -c(1,2)]/100
 region_names <- c("Southern Italy","Sardinia","Northern Italy")
 olives$Region <- factor(region_names[olives$Region])
 names(olives) <- tolower(names(olives))
