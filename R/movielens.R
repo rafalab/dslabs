7fee4533281b98cada1008fc93e828f524c1429c
Rafael A. Irizarry <rairizarry@gmail.com>
2018-07-16 13:25:59 -0400

    fixing an error in the man page for movilens data

--- R/movielens.R
+++ R/movielens.R
@@ -20,7 +20,7 @@
 #'
 #' @keywords datasets
 #' 
-#'  @references 
+#' @references 
 #' F. Maxwell Harper and Joseph A. Konstan. 2015. The MovieLens Datasets: History and Context. ACM Transactions on Interactive Intelligent Systems (TiiS) 5, 4, Article 19 (December 2015), 19 pages. DOI=http://dx.doi.org/10.1145/2827872
 #' 
 #' @source \href{http://files.grouplens.org/datasets/movielens/ml-latest-small.zip}{http://files.grouplens.org/datasets/movielens/ml-latest-small.zip}
