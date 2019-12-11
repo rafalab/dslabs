ae627460015564709d98a99366c76f4324b42d4f
Rafael A. Irizarry <rairizarry@gmail.com>
2019-12-11 12:09:00 -0800

    making data version 2 to avoid dependency on 3.5. Changing my email to official one.

--- inst/script/make-divorce_margarine.R
+++ inst/script/make-divorce_margarine.R
@@ -3,4 +3,4 @@ divorce_margarine<- data.frame(divorce_rate_maine = c(5, 4.7, 4.6, 4.4, 4.3, 4.1
                    margarine_consumption_per_capita = c(8.2, 7, 6.5, 5.3, 5.2, 4, 4.6, 4.5, 4.2, 3.7),
                    year = 2000:2009) 
 
-save(divorce_margarine, file="data/divorce_margarine.rda", compress="xz")
+save(divorce_margarine, file="data/divorce_margarine.rda", compress="xz", version = 2)
