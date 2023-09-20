61cfa81df117f3659b052bfcbe80f2c3d14d3ddc
Rafael A Irizarry <rafa@ds.dfci.harvard.edu>
2023-09-19 20:15:47 -0400

    adding new script

--- inst/script/make-calificaciones.R
+++ inst/script/make-calificaciones.R
@@ -4,9 +4,9 @@ set.seed(2023 - 9 - 10)
 original_time <- Sys.getlocale(category = "LC_TIME")
 olocale <- Sys.getlocale()
 Sys.setlocale("LC_TIME", "es_ES.UTF-8")
-name <- c("Plácido", "João", "Beyoncé", "López", "Thalía", "Blümchen", "Ñengo") 
+nombre <- c("Plácido", "João", "Beyoncé", "López", "Thalía", "Blümchen", "Ñengo") 
 df <- data.frame(
-  name = name,
+  nombre = nombre,
   dob = format(c(make_date(1941, 1, 24),
                  make_date(1931, 6, 10),
                  make_date(1981, 9, 4),
@@ -14,27 +14,29 @@ df <- data.frame(
                  make_date(1971, 8, 26), 
                  make_date(1980, 4, 20),
                  make_date(1981, 12, 15)), "%d de %B de %Y"),
-  stamp = make_datetime(
+  estampa = make_datetime(
     year = year(today()),
     month = month(today()),
     day = day(today()),
-    hour = sample(17:23, length(name), replace = TRUE),
-    min = sample(0:59, length(name), replace = TRUE),
-    sec = sample(0:59, length(name), replace = TRUE),
-    tz = "America/New_York") |> with_tz(tz = "UTC")
+    hour = sample(17:23, length(nombre), replace = TRUE),
+    min = sample(0:59, length(nombre), replace = TRUE),
+    sec = sample(0:59, length(nombre), replace = TRUE),
+    tz = "America/New_York") |> with_tz(tz = "UTC"),
+  puntuación = format(round(runif(length(nombre),80,100),1), decimal.mark = ",")
 )
 
-df <- df[order(df$name),]
+
+df <- df[order(df$nombre),]
 # Write the data frame to CSV in ISO-8859-1 encoding
-write.csv(df, file = "inst/extdata/hw-submission-timestamps.csv", fileEncoding = "ISO-8859-1",
+write.csv(df, file = "inst/extdata/calificaciones.csv", fileEncoding = "ISO-8859-1",
           row.names = FALSE)
 
 ## test
 Sys.setlocale("LC_TIME", original_time)
 
 library(readr)
-x <- read_csv("inst/extdata/hw-submission-timestamps.csv")
-x <- read_csv("inst/extdata/hw-submission-timestamps.csv", locale = locale(encoding = "ISO-8859-1"))
+x <- read_csv("inst/extdata/calificaciones.csv")
+x <- read_csv("inst/extdata/calificaciones.csv", locale = locale(encoding = "ISO-8859-1", decimal_mark = ","))
 parse_date(x$dob, format = "%d de %B de %Y", locale = locale(date_names = "es"))
 parse_date(x$dob, format = "%d de %B de %Y", locale = locale(date_names = "es"))
 with_tz(x$stamp, tz =  Sys.timezone())
